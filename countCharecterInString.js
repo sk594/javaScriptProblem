a11b42750189ba03b4599d0af39bf52d786ff23f
sanjay <skgurjar594@gmail.com>
2021-01-18 12:45:34 +0530

    remove regex and add charcodeat

--- countCharecterInString.js
+++ countCharecterInString.js
@@ -3,12 +3,25 @@ function countChar(str) {
 
     for(let char of str) {
         char = char.toLowerCase();
-        if(/[a-z0-9]/.test(char)){     //add regex for count only number and charecter 
+        // if(/[a-z0-9]/.test(char)){     //add regex for count only number and charecter 
+        //     charObj[char] = ++charObj[char] || 1;
+        // }
+        if(isAlfaNumeric(char)){
             charObj[char] = ++charObj[char] || 1;
         }
     }
     console.log("====",charObj)
     return charObj;
+}
+
+function isAlfaNumeric(char) { //check charecter is alfaNumeric or not
+    code = char.charCodeAt(0);
+    if(!(code > 47 && code < 58) &&   //numeric 0-9
+       !(code > 64 && code < 91) &&   // upper case alfa A-Z
+       !(code > 96 && code < 123)     // lower case alfa a-z
+    ){
+        return false;
+    } return true;
 }
 
 countChar('abcc@! # cCc');
